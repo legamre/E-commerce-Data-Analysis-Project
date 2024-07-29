6e8587d800a8efc4bfcb6536c20bcf4d39be8a4e
legamre <legamres@gmail.com>
2024-07-29 16:43:01 +0200

    Update indexes.sql

--- indexes.sql
+++ indexes.sql
@@ -1,15 +1,9 @@
--- Index on OrderDate in Orders table
 CREATE INDEX idx_orders_orderdate ON Orders(OrderDate);
 
--- Index on ProductID in OrderDetails table
 CREATE INDEX idx_orderdetails_productid ON OrderDetails(ProductID);
 
--- Index on ProductID in Reviews table
 CREATE INDEX idx_reviews_productid ON Reviews(ProductID);
 
--- Index on CustomerID in Orders table
 CREATE INDEX idx_orders_customerid ON Orders(CustomerID);
 
--- Index on ProductID in Reviews table
 CREATE INDEX idx_reviews_productid ON Reviews(ProductID);
-
