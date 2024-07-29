7dac56b3ecab3df10625c22e5e8018c0074dd818
legamre <legamres@gmail.com>
2024-07-29 16:42:32 +0200

    Update indexes-check.sql

--- indexes-check.sql
+++ indexes-check.sql
@@ -1,8 +1,5 @@
--- Check indexes on Orders table
 SHOW INDEX FROM Orders;
 
--- Check indexes on OrderDetails table
 SHOW INDEX FROM OrderDetails;
 
--- Check indexes on Reviews table
 SHOW INDEX FROM Reviews;
