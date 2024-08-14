d6e441908499e3bbcaed59ddd9ecdfe757057c64
legamre <legamres@gmail.com>
2024-08-14 14:23:38 +0200

    Delete total-sales.sql

--- total-sales.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    YEAR(OrderDate) AS Year,
-    MONTH(OrderDate) AS Month,
-    SUM(TotalAmount) AS TotalSales
-FROM Orders
-GROUP BY YEAR(OrderDate), MONTH(OrderDate)
-ORDER BY Year, Month;
