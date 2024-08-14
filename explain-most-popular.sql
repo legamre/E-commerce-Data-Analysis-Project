689cdb1e414ca44d429e4698d31964c64bfe928d
legamre <legamres@gmail.com>
2024-08-14 14:13:26 +0200

    Delete explain-most-popular.sql

--- explain-most-popular.sql
+++
@@ -1,8 +0,0 @@
-EXPLAIN SELECT 
-    p.ProductName,
-    SUM(od.Quantity) AS TotalSold
-FROM OrderDetails od
-JOIN Products p ON od.ProductID = p.ProductID
-GROUP BY p.ProductName
-ORDER BY TotalSold DESC
-LIMIT 5;
