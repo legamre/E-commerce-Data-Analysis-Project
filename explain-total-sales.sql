9396ec0c28905b83b08cb43a525dbbabce78fcba
legamre <legamres@gmail.com>
2024-08-14 14:12:54 +0200

    Delete explain-total-sales.sql

--- explain-total-sales.sql
+++
@@ -1,7 +0,0 @@
-EXPLAIN SELECT 
-    YEAR(OrderDate) AS Year,
-    MONTH(OrderDate) AS Month,
-    SUM(TotalAmount) AS TotalSales
-FROM Orders
-GROUP BY YEAR(OrderDate), MONTH(OrderDate)
-ORDER BY Year, Month;
