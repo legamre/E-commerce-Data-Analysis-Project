7bf9899a883987626d82c374c892cb03f082c449
legamre <legamres@gmail.com>
2024-08-14 14:12:37 +0200

    Delete indexes.sql

--- indexes.sql
+++
@@ -1,9 +0,0 @@
-CREATE INDEX idx_orders_orderdate ON Orders(OrderDate);
-
-CREATE INDEX idx_orderdetails_productid ON OrderDetails(ProductID);
-
-CREATE INDEX idx_reviews_productid ON Reviews(ProductID);
-
-CREATE INDEX idx_orders_customerid ON Orders(CustomerID);
-
-CREATE INDEX idx_reviews_productid ON Reviews(ProductID);
