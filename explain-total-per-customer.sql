367a72f82881941d56f2fd1ae4ca7dbe3c60c524
legamre <legamres@gmail.com>
2024-08-14 14:13:03 +0200

    Delete explain-total-per-customer.sql

--- explain-total-per-customer.sql
+++
@@ -1,8 +0,0 @@
-EXPLAIN SELECT 
-    c.Name,
-    COUNT(o.OrderID) AS TotalOrders,
-    SUM(o.TotalAmount) AS TotalSpent
-FROM Customers c
-JOIN Orders o ON c.CustomerID = o.CustomerID
-GROUP BY c.Name
-ORDER BY TotalSpent DESC;
